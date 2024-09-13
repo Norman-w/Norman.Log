bec6d23676356c3924fcfac5bce8b264c60f0922
Norman <85535885@qq.com>
2024-09-13 19:58:25 +0800

    支持客户端上报日志给服务端接口

--- Norman.Log.Server/CommonFacade/RESTful/LogController.cs
+++ Norman.Log.Server/CommonFacade/RESTful/LogController.cs
@@ -15,9 +15,10 @@ using Norman.Log.Server.Core;
 
 namespace Norman.Log.Server.CommonFacade.RESTful;
 
+#region 日志查询接口
 [ApiController]
 [Route("[controller]")]
-public class LogController : ControllerBase
+public partial class LogController : ControllerBase
 {
 	/// <summary>
 	/// 日志查询参数
@@ -27,42 +28,52 @@ public class LogController : ControllerBase
 		/// <summary>
 		/// 查询的开始时间
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public DateTime? StartTime { get; set; }
 		/// <summary>
 		/// 查询的结束时间
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public DateTime? EndTime { get; set; }
 		/// <summary>
 		/// 所需要查询的日志记录器名称,可能是多个,用逗号或者是空格分隔,需要拆分出来依次匹配,如"logger1,logger2"
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public string? LoggerNameList { get; set; }
 		/// <summary>
 		/// 所需要查询的日志类型,可能是多个,用逗号或者是空格分隔,需要拆分出来依次匹配,如0,1,3
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public string? TypeList { get; set; }
 		/// <summary>
 		/// 所需要查询的日志层级,可能是多个,用逗号或者是空格分隔,需要拆分出来依次匹配,如1,2,4
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public string? LayerList { get; set; }
 		/// <summary>
 		/// 所需要查询的模块名称,可能是多个,用逗号或者是空格分隔,需要拆分出来依次匹配,如"模块1,模块2"
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public string? ModuleList { get; set; }
 		/// <summary>
 		/// 搜索概要和详情时的关键字集,可能用逗号或者是空格分隔,需要拆分出来依次匹配,如"关键字1,关键字2"
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public string? SummaryAndDetailTags { get; set; }
 		/// <summary>
 		/// 上下文信息中包含的关键字集,可能用逗号或者是空格分隔,需要拆分出来依次匹配,如"关键字1,关键字2"
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public string? ContextTags { get; set; }
 		/// <summary>
 		/// 查询的页码,从1开始
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public int PageNumber { get; set; }
 		/// <summary>
 		/// 查询的每页大小
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json反序列化时使用
 		public int PageSize { get; set; }
 	
 		/// <summary>
@@ -123,19 +134,28 @@ public class LogController : ControllerBase
 	}
 	public class LogQueryResponse
 	{
+		// ReSharper disable once MemberCanBePrivate.Global 将被序列化后返回到客户端
 		public string ErrMsg { get; set; } = string.Empty;
 		public bool IsError => !string.IsNullOrWhiteSpace(ErrMsg);
+
 		/// <summary>
 		/// 本次查询到的日志
 		/// </summary>
-		public List<LogRecord4Net> Logs { get; set; }
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json序列化时使用
+		public List<LogRecord4Net> Logs { get; set; } = new();
 		/// <summary>
 		/// 符合条件的总日志数量
 		/// </summary>
+		// ReSharper disable once UnusedAutoPropertyAccessor.Global 将被json序列化时使用
 		public uint TotalResultCount { get; set; }
 	}
-	[HttpPost]
-	public IActionResult Index(LogQueryRequest request)
+	/// <summary>
+	/// 通过给定的参数来查询日志,返回符合条件的日志列表,包含详细信息,不精简字段
+	/// </summary>
+	/// <param name="request"></param>
+	/// <returns></returns>
+	[HttpPost("")]//这里必须要写成[HttpPost + ("")]的形式或其他的每个POST都明确请求的路径,否则在默认不写POST参数的情况下会导致Swagger因路径冲突无法生成
+	public IActionResult GetLogs(LogQueryRequest request)
 	{
 		LogQueryDatabaseArgs queryDatabaseArgs;
 		try
@@ -173,8 +193,19 @@ public class LogController : ControllerBase
 		return Ok(response);
 	}
 
-	[HttpGet]
-	public IActionResult Index(string? id, string? createTime, uint? type, uint? layer, string? loggerNameTags,
+	/// <summary>
+	/// 通过给定的参数来查询日志,返回符合条件的日志列表,包含详细信息,不精简字段
+	/// </summary>
+	/// <param name="id"></param>
+	/// <param name="createTime"></param>
+	/// <param name="type"></param>
+	/// <param name="layer"></param>
+	/// <param name="loggerNameTags"></param>
+	/// <param name="moduleTags"></param>
+	/// <param name="contentTags"></param>
+	/// <returns></returns>
+	[HttpGet("")]//这里可写成[HttpGet],也可以写成[HttpGet("Index")]
+	public IActionResult GetLogs(string? id, string? createTime, uint? type, uint? layer, string? loggerNameTags,
 		string? moduleTags, string? contentTags)
 	{
 		var result = new List<LogRecord4Net>();
@@ -186,3 +217,28 @@ public class LogController : ControllerBase
 		return Ok(new { data=result, success = true, total = 10000, message = $"你的查询参数是: id={id}, createTime={createTime}, type={type}, layer={layer}, loggerNameTags={loggerNameTags}, moduleTags={moduleTags}, contentTags={contentTags}"});
 	}
 }
+#endregion
+
+#region 客户端上报日志给服务端接口
+public partial class LogController
+{
+	/// <summary>
+	/// 客户端通过HTTP POST请求上报日志给服务器
+	/// 地址如:http://localhost:5012/Log/Report
+	/// </summary>
+	/// <param name="logRecord4Net">日志的网络传输模型</param>
+	/// <returns></returns>
+	[HttpPost("Report")]
+	public IActionResult Report(LogRecord4Net logRecord4Net)
+	{
+		var log = Model.Log.FromLogRecord4Net(logRecord4Net);
+		App.Server.HandleLog(this,log);
+		//如果他的请求结果说是要返回json(header里面设置的),那么给它返回json
+		var needJsonResponse = Request.Headers["Accept"].Contains("application/json");
+		if (!needJsonResponse) return Ok();
+		var rspJson = new { Success = true, Message = $"日志{logRecord4Net.Id}已经被成功接收于{DateTime.Now}" };
+		return Ok(rspJson);
+	}
+}
+
+#endregion
