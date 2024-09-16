9076adea12ed06106a7b23bf04ac60eda0629d2d
Norman <85535885@qq.com>
2024-09-16 10:47:33 +0800

    添加Norman.Log.Logger.HTTP的测试用例

---
+++ LoggerHTTPTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using Norman.Log.Logger.HTTP;
+
+namespace LoggerHTTPTest
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			Logger.UpdateConfig("http://localhost:5012/Log/Report", "DefaultLogger");
+			Logger.Write(LogTypeEnum.Info, LogLayerEnum.Business, "TestModule", "TestSummary", "TestDetail");
+		}
+	}
+}
