703d36bcaf21c472eb6bfaa4035d6a01daad9f59
Norman <85535885@qq.com>
2024-12-15 18:37:20 +0800

    "优化全局配置加载逻辑，添加延迟初始化以提升性能"

--- Norman.Log.Server/App.cs
+++ Norman.Log.Server/App.cs
@@ -12,14 +12,39 @@ public static class App
 	/// </summary>
 	internal static readonly Core.Server Server = new();
 
+	#region 全局配置
+
+	#region LoggerConfig
+
+	private static LoggerConfig? _loggerConfig;
+
+	private static LoggerConfig LoadLoggerConfig()
+	{
+		return ConfigFactory.CreateFromFile<LoggerConfig>("LoggerConfig.config", true);
+	}
+
 	/// <summary>
 	///     日志记录器设置
 	/// </summary>
-	public static LoggerConfig LoggerConfig { get; } =
-		ConfigFactory.CreateFromFile<LoggerConfig>("LoggerConfig.config", true);
+	public static LoggerConfig LoggerConfig { get; } = _loggerConfig ??= LoadLoggerConfig();
+
+	#endregion
+
+	#region Setting
+
+	private static Setting? _setting;
+
+	private static Setting LoadSetting()
+	{
+		return ConfigFactory.CreateFromFile<Setting>("Setting.config", true);
+	}
 
 	/// <summary>
-	/// 应用程序主设置,也就是Server的设置
+	///    应用程序主设置,也就是Server的设置
 	/// </summary>
-	internal static Setting Setting { get; } = ConfigFactory.CreateFromFile<Setting>("Setting.config", true);
+	internal static Setting Setting { get; } = _setting ??= LoadSetting();
+
+	#endregion
+
+	#endregion
 }
