23fc8d825e350644e38fc2eae154baaf1ca6d85f
Norman <85535885@qq.com>
2024-12-15 18:58:07 +0800

    "新增Setting类注释及增强方法，以支持默认设置生成、从JSON字符串填充和从文件加载功能"

--- Norman.Log.Server/Setting.cs
+++ Norman.Log.Server/Setting.cs
@@ -9,14 +9,30 @@ namespace Norman.Log.Server;
 /// </summary>
 internal class Setting : ICommonConfig<Setting>
 {
+	/// <summary>
+	/// 通过grpc接收日志的端口
+	/// </summary>
 	public int GrpcPort { get; set; }
 
+	/// <summary>
+	/// 通过grpc-web接收日志的端口
+	/// </summary>
 	public int GrpcWebPort { get; set; }
 
+	/// <summary>
+	/// 控制面板设置
+	/// </summary>
 	public ControlPanelSetting ControlPanel { get; set; } = new();
 
+	/// <summary>
+	/// 日志接收器设置,服务器要把日志分发给哪些客户端的相关设置
+	/// </summary>
 	public ReceiverSetting Receiver { get; set; } = new();
 
+	/// <summary>
+	/// 获取默认设置
+	/// </summary>
+	/// <returns></returns>
 	public Setting GetDefault()
 	{
 		return new Setting
@@ -39,11 +55,20 @@ internal class Setting : ICommonConfig<Setting>
 		};
 	}
 
+	/// <summary>
+	/// 从json字符串中填充数据
+	/// </summary>
+	/// <param name="json"></param>
 	public void Populate(string json)
 	{
 		JsonConvert.PopulateObject(json, this);
 	}
 
+	/// <summary>
+	/// 从文件中读取设置
+	/// </summary>
+	/// <param name="path"></param>
+	/// <param name="tryCreateIfNotExist"></param>
 	public void FromFile(string path, bool tryCreateIfNotExist = true)
 	{
 		if (tryCreateIfNotExist && !File.Exists(path))
